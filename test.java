6042c9da3c9d7a995d178e6213281bf0057bca3a
Romyshufman <romyshufman@mail.tau.ac.il>
2022-05-21 15:27:01 +0300

    Test 3.1

--- test.java
+++ test.java
@@ -4,11 +4,16 @@ public class test {
 
     public static void main (String[] args) {
 
-        IHashTable table = new QPHashTable(6571, 6571);
+    }
+
+    public static void q3_1(){
+        IHashTable table = new DoubleHashTable(6571, 6571);
         int count = 0;
         for (int i = 0; i <= 6571; i++) {
-            long key = (long) (Math.pow(i, 2) % 6571);
-            HashTableElement elem = new HashTableElement(key, i);
+            long key= (long) Math.pow(i, 2);
+            //long key = (long) (Math.pow(-1,i)*(Math.pow(i, 2)));
+            long final_key=  ((key%6571)+6571)%6571;
+            HashTableElement elem = new HashTableElement(final_key, i);
             try {
                 table.Insert(elem);
                 count++;
