e91fd28688636bfeb18a3c41c921912ac4e35fb3
noahadash1 <noahadash@mail.tau.ac.il>
2022-05-21 13:59:12 +0300

    ModHash GetFunc

--- ModHash.java
+++ ModHash.java
@@ -5,21 +5,26 @@ public class ModHash {
 
 	private long a;
 	private long b;
+	private int m;
+	private long p;
 
-	public ModHash(long a, long b){
-		this.a=a;
-		this.b=b;
+	public ModHash(long a, long b, int m, long p){
+		this.a = a;
+		this.b = b;
+		this.p = p;
+		this.m = m;
 	}
 	
 	public static ModHash GetFunc(int m, long p){
 		Random rand= new Random();
 		long a= ThreadLocalRandom.current().nextLong(1, p);
 		long b=  ThreadLocalRandom.current().nextLong(0, p);
-		return new ModHash(a,b);
+		return new ModHash(a, b, m, p);
 	}
 	
 	public int Hash(long key) {
 		// TODO implement hash function
-		return 0;
+		int result = (int) (((a*key+b)%p)%m);
+		return result;
 	}
 }
